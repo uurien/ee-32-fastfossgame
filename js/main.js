4ea10774bed012fc6a5206bbd9cd72ac0476fa12
Ugaitz Urien <ugaitz.urien@gmail.com>
2024-07-25 23:41:37 +0200

    Apply @arkka9 pictures

--- js/main.js
+++ js/main.js
@@ -20,30 +20,50 @@
             update: update
         }
     };
-
-    setInterval(() => {
-        speed += .2;
-    }, 500)
+    //
+    // setInterval(() => {
+    //     speed += .2;
+    // }, 5000)
 
     const game = new Phaser.Game(config);
-    let background;
+    const backgrounds = [];
     let ground;
     let player;
     let spaceBar;
-    let speed = 0.1;
+    let speed = 10;
     const platforms = []
     function preload() {
         this.load.image('background_0', 'assets/background_0.jpg');
+        this.load.image('background_1', 'assets/background_1.png');
+        this.load.image('background_2', 'assets/background_2.png');
         // this.load.image('sky', 'assets/Fondo_noche.ase');
         this.load.image('player', 'assets/player.png');  // Asegúrate de tener esta imagen en la carpeta assets
-        this.load.image('ground', 'assets/player.png');  // Asegúrate de tener esta imagen en la carpeta assets
+        this.load.image('ground', 'assets/ground.png');  // Asegúrate de tener esta imagen en la carpeta assets
 
     }
 
     function create() {
-        background = this.add.tileSprite(0, 0, this.sys.canvas.width, this.sys.canvas.height, 'background_0');
+        let background = this.add.tileSprite(0, 0, this.sys.canvas.width, this.sys.canvas.height, 'background_0');
         background.setOrigin(0, 0);
         background.setScrollFactor(0);
+        backgrounds.push({
+            tile: background,
+            speedFactor: 0.01
+        })
+        let background1 = this.add.tileSprite(0, 0, this.sys.canvas.width, this.sys.canvas.height, 'background_1');
+        background1.setOrigin(0, 0);
+        background1.setScrollFactor(0);
+        backgrounds.push({
+            tile: background1,
+            speedFactor: 0.03
+        })
+        let background2 = this.add.tileSprite(0, 0, this.sys.canvas.width, this.sys.canvas.height, 'background_2');
+        background2.setOrigin(0, 0);
+        background2.setScrollFactor(0);
+        backgrounds.push({
+            tile: background2,
+            speedFactor: 0.06
+        })
 
 //        grown = this.physics.add.staticImage(0, 568, 'platform').setScale(2).refreshBody();
         ground = this.add.tileSprite(0, this.sys.canvas.height - 32, this.sys.canvas.width, 32, 'ground');
@@ -78,7 +98,7 @@
     }
 
     function update() {
-        background.tilePositionX += speed;
+        moveBackgrounds()
         // for (let i = 0; i < platforms.length; i++) {
         //     const platform = platforms[i]
         //     // console.log(platform.tilePositionX)
@@ -89,6 +109,13 @@
         if (spaceBar.isDown && player.body.touching.down) {
             console.log('ey')
             player.setVelocityY(-530);  // Ajusta el valor para cambiar la altura del salto
+        }
+    }
+
+    function moveBackgrounds () {
+        for (let i = 0; i < backgrounds.length; i++) {
+            const background = backgrounds[i]
+            background.tile.tilePositionX += background.speedFactor * speed;
         }
     }
 })()
