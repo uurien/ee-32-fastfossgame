feaa959533314ff9cd1a7c163893e9e8c8d4d503
Ugaitz Urien <ugaitz.urien@gmail.com>
2024-07-26 13:47:18 +0200

    Musica y un poco de random

--- js/main.js
+++ js/main.js
@@ -15,11 +15,20 @@
             document.getElementById('about').classList.remove('hidden')
         }
     }
-    let intervalsToCleanOnDestroy = []
+    let timeoutsToCleanOnDestroy = []
+
+    const platformLevelHeight = 100;
+    const platformLevels = 6;
+    let currentLevel = 0;
 
     function start () {
+        function getTimeoutMs () {
+            return 4000 - (speed * 5)
+        }
+
+        currentLevel = 0;
         if (window.GAME.currentGame) {
-            intervalsToCleanOnDestroy.forEach(clearInterval)
+            timeoutsToCleanOnDestroy.forEach(clearTimeout)
             window.GAME.currentGame.destroy(true)
             window.GAME.currentGame = null
         }
@@ -31,7 +40,7 @@
             physics: {
                 default: 'arcade',
                 arcade: {
-                    gravity: { y: 1200 },
+                    gravity: { y: 2200 },
                     debug: false
                 }
             },
@@ -48,6 +57,9 @@
         //     document.getElementById('main-menu').classList.add('hidden')
         // }
         window.GAME.gameOver = () => {
+            if (backgroundMusic) {
+                backgroundMusic.stop();
+            }
             game.pause();
             document.getElementById('main-menu').classList.remove('hidden')
 
@@ -56,7 +68,8 @@
         let ground;
         let player;
         let spaceBar;
-        let speed = 10;
+        let speed = 20;
+        let backgroundMusic;
         const platforms = []
         function preload() {
             this.load.image('background_0', 'assets/background_0.jpg');
@@ -64,6 +77,7 @@
             this.load.image('background_2', 'assets/background_2.png');
             this.load.image('player', 'assets/player.png');
             this.load.image('ground', 'assets/ground.png');
+            this.load.audio('background_music', 'assets/background_music.mp3');
         }
 
         function create() {
@@ -107,10 +121,22 @@
             spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
             const addPlatform = () => {
-                const platform = this.physics.add.sprite( 1200, 500, 'ground');
+                if (Math.random() < .05) {
+                    console.log('do not add')
+                    timeoutsToCleanOnDestroy.push(setTimeout(addPlatform, getTimeoutMs()))
+                    return
+                }
+                const playerLevel = Math.floor((600 - player.y) / platformLevelHeight)
+                const rand = 3 * (Math.random() - 0.5)
+                console.log('rand', rand)
+                let platformLevel = playerLevel + Math.floor(rand)
+                platformLevel = platformLevel < 0 ? 0: platformLevel;
+                console.log(platformLevel)
+                const y = 600 - (platformLevel * platformLevelHeight) - 48
+                const platform = this.physics.add.sprite( 1200, y, 'ground');
                 platform.setImmovable(true);
                 platform.body.allowGravity = false;
-                platform.setVelocityX(-200)
+                platform.setVelocityX(-20*speed)
                 platform.displayWidth = 300;
                 platform.body.setFriction(0, 0);
                 // platform.body.setSize(300, platform.displayHeight); // Ajustar el cuerpo de colisión
@@ -121,11 +147,16 @@
                 });
 
                 platforms.push(platform)
+
+
+                timeoutsToCleanOnDestroy.push(setTimeout(addPlatform, getTimeoutMs()))
             }
             addPlatform()
-            intervalsToCleanOnDestroy.push(setInterval(addPlatform, 5_000))
 
             document.getElementById('main-menu').classList.add('hidden')
+
+            backgroundMusic = this.sound.add('background_music');
+            backgroundMusic.play({ loop: true });
         }
 
         function update() {
@@ -133,7 +164,7 @@
             player.setVelocityX(0);
 
             if (spaceBar.isDown && player.body.touching.down) {
-                player.setVelocityY(-530);  // Ajusta el valor para cambiar la altura del salto
+                player.setVelocityY(-800);  // Ajusta el valor para cambiar la altura del salto
             }
         }
 
